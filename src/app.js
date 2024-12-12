c52b0a5b34b94db24d7a030e5cc2c295b9a249ed
Lor777 <lorenzosifuentes09@gmail.com>
2024-12-12 16:59:54 -0600

    Agregar url backend render

--- src/app.js
+++ src/app.js
@@ -24,8 +24,8 @@ app.use(cors({
     origin: [
     'http://localhost:5173', 
     'http://localhost:4000',
-    'http://localhost'
-    ,process.env.BASE_URL],
+    'http://localhost',
+    process.env.BASE_URL],
     credentials:true
 }));
 app.use(morgan('dev'));
