7cda5da76c5b657a2cf8b220de678ba60847f8b4
cdlhza <cdl.hza@skiff.com>
2024-12-15 17:10:18 -0600

    Inicializando el Backend

--- src/db.js
+++ src/db.js
@@ -1,14 +1,13 @@
-import mongoose from  'mongoose';
+import mongoose from "mongoose";
 
-export const connectDB = async ()=>{
-  
-        const url = process.env.MONGODB_URL;
-          // await mongoose.connect('mongodb://127.0.0.1/sistema');
-       await mongoose.connect(url)
-       .then(()=>{
-        console.log('Base de datos conetada hola')
-       })
-    .catch ((err)=>{
-        console.log(error);
-    })
-}
+export const connectDB = async () => {
+  try {
+    const url =
+      "mongodb+srv://Daniel:36442812@solik.7tdvw.mongodb.net/?retryWrites=true&w=majority&appName=Solik";
+    //await mongoose.connect('mongodb://127.0.0.1/sistema');
+    await mongoose.connect(url);
+    console.log("Base de datos conetada con exito");
+  } catch (error) {
+    console.log(error);
+  }
+};
