4e2224f3a7f957c49921aae711d1106579bc3038
cdlhza <cdl.hza@skiff.com>
2024-12-17 23:02:19 -0600

    Actualizacion 1.1

--- src/libs/jwt.js
+++ src/libs/jwt.js
@@ -3,12 +3,19 @@ import { TOKEN_SECRET } from "../config.js";
 
 export function createAccessToken(payload) {
   return new Promise((resolve, reject) => {
-    jwt.sign(payload, TOKEN_SECRET, (err, token) => {
-      if (err) {
-        reject(err);
-        console.log(err);
+    jwt.sign(
+      payload,
+      TOKEN_SECRET,
+      {
+        expiresIn: "1d",
+      },
+      (err, token) => {
+        if (err) {
+          reject(err);
+          console.log(err);
+        }
+        resolve(token);
       }
-      resolve(token);
-    }); //fin del sin hola
+    ); //fin del sin hola
   }); //fin de Promise
 } //fin de creatAccessToken
